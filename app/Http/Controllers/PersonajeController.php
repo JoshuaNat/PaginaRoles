27929b0ecc5cdf8bd48f2984bc795e8b76a254b1
Joshua Lopez <xyznathaniel@gmail.com>
2021-06-26 01:55:50 -0500

    modelo, migracion y controlador de CRUD

--- app/Http/Controllers/PersonajeController.php
+++ app/Http/Controllers/PersonajeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Historia;
+use App\Models\Personaje;
 use Illuminate\Http\Request;
 
-class HistoriaController extends Controller
+class PersonajeController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -41,10 +41,10 @@ class HistoriaController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Historia  $historia
+     * @param  \App\Models\Personaje  $personaje
      * @return \Illuminate\Http\Response
      */
-    public function show(Historia $historia)
+    public function show(Personaje $personaje)
     {
         //
     }
@@ -52,10 +52,10 @@ class HistoriaController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Historia  $historia
+     * @param  \App\Models\Personaje  $personaje
      * @return \Illuminate\Http\Response
      */
-    public function edit(Historia $historia)
+    public function edit(Personaje $personaje)
     {
         //
     }
@@ -64,10 +64,10 @@ class HistoriaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Historia  $historia
+     * @param  \App\Models\Personaje  $personaje
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Historia $historia)
+    public function update(Request $request, Personaje $personaje)
     {
         //
     }
@@ -75,10 +75,10 @@ class HistoriaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Historia  $historia
+     * @param  \App\Models\Personaje  $personaje
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Historia $historia)
+    public function destroy(Personaje $personaje)
     {
         //
     }
---
+++ database/migrations/2021_06_26_063015_create_personajes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonajesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personajes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre', 30);
+            $table->smallInteger('Edad');
+            $table->string('Genero', 10);
+            $table->text('Personalidad');
+            $table->text('Historia');
+            $table->text('Extras');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('historia_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personajes');
+    }
+}
