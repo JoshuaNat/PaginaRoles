cd607236d8da1231f368abf85b516783388cd590
Joshua Lopez <xyznathaniel@gmail.com>
2021-06-25 19:58:44 -0500

    migracion y modelo de historia

---
+++ database/migrations/2021_06_26_003445_create_historias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historias', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre');
+            $table->text('Descripcion');
+            $table->string('Genero');
+            $table->string('Estado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historias');
+    }
+}
