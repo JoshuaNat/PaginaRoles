9bc7b734136fbc5ae269f22e1cbd627c21a25aca
Joshua Lopez <xyznathaniel@gmail.com>
2021-06-29 19:38:53 -0500

    verificacion de email

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonajeController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,22 @@ use App\Http\Controllers\PersonajeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::get('inicio', function () {
     return view('inicio');
@@ -27,4 +46,4 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::post('personaje/{personaje}/AgregaHistoria', [PersonajeController::class, 'agregaHistoria'])->name('personaje.agregaHistoria');
-Route::resource('personaje', PersonajeController::class)->middleware('auth');
+Route::resource('personaje', PersonajeController::class)->middleware('verified');
